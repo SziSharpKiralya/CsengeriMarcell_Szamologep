529f4461709b102f385ceecf38a359a75b28e015
Csengeri Marcell <csengeri.marcell@petrik.hu>
2025-01-20 13:10:51 +0100

    Indit() kész

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -9,10 +9,74 @@ namespace ConsoleApp3
     internal class Program
     {
 
+        static float num0 = 0;
+        static float num1 = 0;
+        static string operation = "";
+
+        static void Osszeadas()
+        {
+
+        }
+
+        static void Kivonas()
+        {
+
+        }
+
+        static void Osztas()
+        {
+            
+        }
+
+        static void Szorzas()
+        {
+
+        }
+
+        static void Szazalek()
+        {
+
+        }
+
+        static void Sugo()
+        {
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Műveletek jelei:");
+            Console.WriteLine("Összeadás: +");
+            Console.WriteLine("Kivonas: -");
+            Console.WriteLine("Osztas: / vagy :");
+            Console.WriteLine("Szorzás: * vagy x");
+            Console.WriteLine("Százalék: %");
+            Indit();
+        }
+
+        static void Indit()
+        {
+            Console.WriteLine("---------------------------------");
+            Console.Write("Kérem adja meg az első számot: ");
+            num0 = float.Parse(Console.ReadLine());
+            Console.Write("Kérem adja meg a második számot: ");
+            num1 = float.Parse(Console.ReadLine());
+            Console.Write("Az elvégzendő művelet jele: ");
+            operation = Console.ReadLine();
+            Console.WriteLine($"{num0}, {num1}, {operation}");
+
+            switch (operation)
+            {
+                case "+": Osszeadas(); break;
+                case "-": Kivonas(); break;
+                case "/": Osztas(); break;
+                case ":": Osztas(); break;
+                case "*": Szorzas(); break;
+                case "x": Szorzas(); break;
+                case "%": Szazalek(); break;
+                case "?": Sugo(); break;
+            }
+        }
 
         static void Main(string[] args)
         {
-
+            Indit();
         }
     }
 }
